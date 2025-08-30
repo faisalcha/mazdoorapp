efebf0c9dea60c5bfeadca7f49baa5706fa3e2d3
faisalcha <faisal.chatha@gmail.com>
2025-08-31 00:47:31 +0500

    Inject RedisService into JobsController

--- mazdoorhub-v2-fixed-repo/backend/src/integrations/integrations.module.ts
+++ mazdoorhub-v2-fixed-repo/backend/src/integrations/integrations.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { S3Service } from './s3.service';
+
 import { PushService } from './push.service';
+import { RedisService } from './redis.service';
+import { S3Service } from './s3.service';
 import { SmsService } from './sms.service';
 import { OrsService } from './ors.service';
-import { RedisService } from './redis.service';
-@Module({ providers:[S3Service,PushService,SmsService,OrsService,RedisService], exports:[S3Service,PushService,SmsService,OrsService,RedisService] })
+
+@Module({
+  providers: [S3Service, PushService, SmsService, OrsService, RedisService],
+  exports: [S3Service, PushService, SmsService, OrsService, RedisService],
+})
 export class IntegrationsModule {}
